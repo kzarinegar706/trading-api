7a08a777e5658d7071bd8fe0a19ec89a51fdf330
kade <kzarinegar706@west-mec.org>
2021-03-24 14:35:47 -0700

    callback url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 
 
 var app = express();
-const redirect_uri = 'http://localhost';
+const redirect_uri = 'https://<tradingAPI>.herokuapp.com/auth';
 
 /* 
 Callback endpoint the TDA app uses.
